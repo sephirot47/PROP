6a47ec69b34103b1e5b79ef3fd90461b28d6af08
Abraham Cortès Vicent <doros_1@hotmail.com>
2015-04-29 19:54:27 +0200

    Ponderacions i reproducions test OK

--- src/Domini/ReproductionTest.java
+++ src/Domini/ReproductionTest.java
@@ -14,8 +14,51 @@ public class ReproductionTest extends TestCase
 		super.setUp();
 	}
 
-	public void test() 
+	public void TestPrint()
 	{
-		assertEquals(111, 111);
+		Reproduction r = new Reproduction("AC/DC","T.N.T",101);
+		r.Print();
+		
 	}
+	
+	public void TestGetSongAuthor() 
+	{
+		Reproduction r = new Reproduction("AC/DC","T.N.T",101);
+		assertEquals("AC/DC",r.GetSongAuthor());
+	}
+
+	public void TestSetSongAuthor() 
+	{
+		Reproduction r = new Reproduction("Justin Bebier","T.N.T",101);
+		r.SetSongAuthor("AC/DC");
+		assertEquals("AC/DC",r.GetSongAuthor());
+		
+	}
+
+	public void TestGetSongTitle() 
+	{
+		Reproduction r = new Reproduction("AC/DC","Highway to hell",208);
+		assertEquals("Highway to hell",r.GetSongTitle());
+	}
+
+	public void TestSetSongTitle() 
+	{
+		Reproduction r = new Reproduction("AC/DC","La Macarena",208);
+		r.SetSongTitle("Highway to hell");
+		assertEquals("Highway to hell",r.GetSongTitle());
+		
+	}
+
+	public void TestGetTime() 
+	{
+		Reproduction r = new Reproduction("AC/DC","Thunderstruck",294);
+		assertEquals(294,r.GetTime());
+	}
+
+	public void TestSetTime() {
+		Reproduction r = new Reproduction("AC/DC","Thunderstruck",1);
+		r.SetTime(294);
+		assertEquals(294,r.GetTime());
+	}
+
 }
