69dc77aec85f5738f80942665b8006a861e4b1ef
sephirot47 <victoriodesica777@hotmail.com>
2015-04-29 09:38:30 +0200

    Pair Test DONE

--- src/Domini/PairTest.java
+++ src/Domini/PairTest.java
@@ -14,8 +14,38 @@ public class PairTest extends TestCase
 		super.setUp();
 	}
 
-	public void test() 
+	public void testSetFirst() 
 	{
-		assertEquals(111, 111);
+		Pair<Integer, Boolean> p = new Pair<Integer, Boolean>();
+		p.SetFirst(42);
+		assertEquals(42, (int)p.GetFirst());
+	}
+
+	public void testSetSecond() 
+	{
+		Pair<Integer, Float> p = new Pair<Integer, Float>();
+		p.SetSecond(2347.12732f);
+		assertEquals(2347.12732f, (float)p.GetSecond());
+	}
+
+	public void testGetFirst() 
+	{
+		Pair<Integer, Float> p = new Pair<Integer, Float>();
+		p.SetFirst(347);
+		assertEquals(347, (int)p.GetFirst());
+	}
+	
+	public void testGetSecond() 
+	{
+		Pair<Integer, Float> p = new Pair<Integer, Float>();
+		p.SetSecond(347.2347f);
+		assertEquals(347.2347f, (float)p.GetSecond());
+	}
+
+	public void testPairConstructor() 
+	{
+		Pair<Integer, Float> p = new Pair<Integer, Float>(42, 3.141592f);
+		assertEquals(42,        (int)  p.GetFirst());
+		assertEquals(3.141592f, (float)p.GetSecond());
 	}
 }
