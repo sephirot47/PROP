6ba3f602db9eaadc4f4b10bbbe7d613d6da516dc
Abraham4 <doros_1@hotmail.com>
2015-04-29 20:03:11 +0200

    Update PonderationsTest.java

--- src/Domini/PonderationsTest.java
+++ src/Domini/PonderationsTest.java
@@ -14,8 +14,85 @@ public class PonderationsTest extends TestCase
 		super.setUp();
 	}
 
-	public void test() 
+
+	
+	public void TestGetThreshold()
 	{
-		assertEquals(111, 111);
+		Ponderations t = new Ponderations();
+		
+		assertEquals(5.0,t.GetThreshold());
 	}
+
+	public void TestGetDuration() {
+		
+		Ponderations t = new Ponderations();
+		assertEquals(5,t.GetDuration());
+	
+	}
+
+	public void TestSetDuration() {
+		Ponderations t = new Ponderations();
+		
+		t.SetDuration(8);
+		assertEquals(8,t.GetDuration());
+		
+	}
+
+	public void TestGetYear() {
+		Ponderations t = new Ponderations();
+		assertEquals(5,t.GetYear());
+		
+	}
+
+	public void TestSetYear() {
+		Ponderations t = new Ponderations();
+		t.SetYear(8);
+		assertEquals(8,t.GetYear());
+	}
+
+	public void TestGetStyle() {
+		Ponderations t = new Ponderations();
+		assertEquals(5, t.GetStyle());
+	}
+
+	public void TestSetStyle(int style) {
+		Ponderations t = new Ponderations();
+		t.SetStyle(8);
+		assertEquals(8,t.GetStyle());
+	}
+
+	public void TestGetUserAge() {
+		Ponderations t = new Ponderations();
+		assertEquals(5,t.GetUserAge());
+	}
+
+	public void TestSetUserAge() {
+		Ponderations t = new Ponderations();
+		t.SetUserAge(33);
+		assertEquals(33,t.GetUserAge());
+	}
+
+	public void TestGetNearbyReproductions() {
+		Ponderations t = new Ponderations();
+		assertEquals(5,t.GetNearbyReproductions());
+	}
+
+	public void TestSetNearbyReproductions() {
+		Ponderations t = new Ponderations();
+		t.SetNearbyReproductions(8);
+		assertEquals(8,t.GetNearbyReproductions());
+	}
+
+	public void TestGetAuthor() {
+		Ponderations t = new Ponderations();
+		assertEquals(5,t.GetAuthor());
+	}
+
+	public void TestSetAuthor() {
+		Ponderations t = new Ponderations();
+		t.SetAuthor(25);
+		assertEquals(25,t.GetAuthor());
+	}
+
 }
+
