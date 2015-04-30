8582224c3c22f42048c0563b67308dfc0f8be466
Aina Soler <ainasoga@gmail.com>
2015-05-01 00:27:17 +0200

    All the default tests for the FileManager Done

--- src/Persistencia/FileManager.java
+++ src/Persistencia/FileManager.java
@@ -178,6 +178,7 @@ public class FileManager
         fileToLoad = fileToLoad.toAbsolutePath();
         path = fileToLoad.toString();
         FileWriter w = new FileWriter(path, true);
+        w.write("\r\n");
         w.write(newLine);
         w.write("\r\n");
         w.close();
