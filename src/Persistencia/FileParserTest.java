384f76e5c59f9b3c363725669e135d0b1eab4f2c
sephirot47 <victoriodesica777@hotmail.com>
2015-04-30 23:08:02 +0200

    Doing FileParser Test

--- src/Persistencia/FileParserTest.java
+++ src/Persistencia/FileParserTest.java
@@ -2,6 +2,8 @@ package Persistencia;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import Domini.Edge;
 import Domini.Graph;
@@ -9,8 +11,10 @@ import Domini.Node;
 import Domini.Reproduction;
 import Domini.Song;
 import Domini.SongGraph;
+import Domini.SongManager;
 import Domini.SongRelation;
 import Domini.User;
+import Domini.UserManager;
 import junit.framework.TestCase;
 
 public class FileParserTest extends TestCase 
@@ -27,14 +31,101 @@ public class FileParserTest extends TestCase
 
 	public void testGetUsers() throws IOException
 	{
+		Set<User> usersRead = UserManager.GetUsers("tests/users1.txt", "tests"); //read from file
+		Set<User> users = new HashSet<User>(); //created by us
+
+		assertFalse(users.equals(usersRead));
+		
+		User u = new User("a", 12);
+		u.AddReproduction(new Reproduction("a", "b", 20));
+		u.AddReproduction(new Reproduction("a", "b", 30));
+		users.add(u);
+		assertFalse(SetsEquals(users, usersRead));
+		
+		u.AddReproduction(new Reproduction("a", "b", 592));
+		assertTrue(SetsEquals(users, usersRead));
+		
+		users.add(new User("xza", 123));
+		assertFalse(SetsEquals(users, usersRead));
+		
+		//TEST 2
+		usersRead.clear();
+		usersRead  = UserManager.GetUsers("tests/users2.txt", "tests"); //read from file
+		users.clear();
+
+		User u2 = new User("BbsdA124^!", 78);
+		u2.AddReproduction(new Reproduction("xcvxcv", "xcvvc", 20));
+		u2.AddReproduction(new Reproduction("xcvxcv", "ioppoi", 30));
+		users.add(u2);
+		assertFalse(SetsEquals(users, usersRead));
+		
+		User u3 = new User("a", 12);
+		u3.AddReproduction(new Reproduction("a", "b", 20));
+		u3.AddReproduction(new Reproduction("a", "b", 30));
+		u3.AddReproduction(new Reproduction("a", "b", 592));
+		users.add(u3);
+		assertFalse(SetsEquals(users, usersRead));
+
+		User u4 = new User("cxcvxcv asdas", 25);
+		users.add(u4);
+		assertFalse(SetsEquals(users, usersRead));
+		
+		u4.AddReproduction(new Reproduction("jkll", "kl", 20));
+		u4.AddReproduction(new Reproduction("jjjj", "jk", 30));
+		assertTrue(SetsEquals(users, usersRead));
+
+		users.add(new User("asdsad", 2));
+		assertFalse(SetsEquals(users, usersRead));
+		
+		users.clear();
+		users.add(new User("xccvxxcv", 6));
+		assertFalse(SetsEquals(users, usersRead));
 	}
 
 	public void testGetSongs() throws IOException
 	{
+		//TEST 1
+		Set<Song> songsRead = SongManager.GetSongs("tests/songs1.txt"); //read from file
+		Set<Song> songs = new HashSet<Song>(); //created by us
+
+		assertFalse(SetsEquals(songs, songsRead)); 
+		
+		ArrayList<String> styles1 = new ArrayList<String>(); styles1.add("estil3");
+		songs.add(new Song("a", "b", 2015, styles1, 42));
+		
+		assertTrue(SetsEquals(songs, songsRead));
+		
+		songs.add(new Song("xza", "b123", 22215, styles1, 678));
+		assertFalse(SetsEquals(songs, songsRead));
+		
+		//TEST 2
+		songsRead.clear();
+		songsRead = SongManager.GetSongs("tests/songs2.txt"); //read from file
+		songs.clear();
+		
+		ArrayList<String> styles20 = new ArrayList<String>(); 
+		styles20.add("s3"); styles20.add("s1"); styles20.add("s2"); 
+		
+		ArrayList<String> styles30 = new ArrayList<String>(); 
+		styles30.add("s1"); styles30.add("s43534");
+		
+		songs.add(new Song("a", "b", 2015, new ArrayList<String>(), 42));
+		songs.add(new Song("c", "t", 2015, styles20, 212));
+		songs.add(new Song("x", "y", 2015, styles30, 212));
+		
+		assertTrue(SetsEquals(songs, songsRead));
+		
+		songs.add(new Song("asdsad", "asddas", 202115, styles20, 345));
+		assertFalse(SetsEquals(songs, songsRead));
+		
+		songs.clear();
+		songs.add(new Song("xccvxxcv", "xcv", 45, styles30, 2));
+		assertFalse(SetsEquals(songs, songsRead));
 	}
 
 	public void testGetReproductions() throws IOException
 	{
+		ArrayList<Reproduction> reprosRead = FileParser.GetReproductions("tests/repros");
 	}
 	
 	public void testGetGraph() throws IOException
@@ -105,4 +196,16 @@ public class FileParserTest extends TestCase
 		Song s2bis = new Song("x", "y", 2015, styles30, 212);
 		assertEquals(s2, s2bis);
     }
+
+	
+	public <T> boolean SetsEquals(Set<T> ss1, Set<T> ss2)
+	{
+		//S'ha de passar a ArrayList perque si no, al comparar sets amb set.equals(set2), tambe compara
+		//els hashcodes, i nomes volem que compari amb la funcio T.equals, per aixo aquesta conversio.
+		ArrayList<T> ar1 = new ArrayList<T>(ss1);
+		ArrayList<T> ar2 = new ArrayList<T>(ss2);
+		
+		//No uso equals, ja que al venir de sets el ordre no importa 
+		return ar1.containsAll(ar2) && ar2.containsAll(ar1); //NO CAL TEST, ES UN AXIOMA DE TEORIA DE CONJUNTS
+	}
 }
